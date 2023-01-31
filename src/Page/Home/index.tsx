498471e35903d4ea51e1a3487e95d9a6ba801d19
Mike <mikeiaspires23@gmail.com>
2023-01-30 21:23:34 -0300

    tentativas

--- src/Page/Home/index.tsx
+++ src/Page/Home/index.tsx
@@ -6,27 +6,43 @@ export default function Home() {
 
     const [storageName, setStorageName] = useState('')
     const [users, setUsers] = useState([])
-    const [item, setItem] = useState([])
+    const [filtedUser, setFiltedUser] = useState([])
+    const [filterNames, setFilterNames] = useState([])
+    
+   
 
-    const imprimir = item.length > 0 ? (item) : (users);
-
-    function HandleName() {
-        const filtros = users.map(item => item.name).filter(item => item.first == storageName);
-        const filtrosName = filtros.find(item => item.first);
-        setItem(filtrosName)
+        function handleSearch() {
+        const Filtered = users.filter(item => item.name.first === storageName);
+        setFiltedUser(Filtered);
         if( storageName == ""){
-        return alert ('Insira um nome')
-        }else {
-                setStorageName("");    
-                console.log(filtrosName)
-            }  
+         return alert ('Insira um nome')
+         }else {
+         setStorageName("");    
+        }  
         }
-     
+   
+
     useEffect (() => {
     fetch("https://randomuser.me/api/?seed=javascript&results=100&nat=BR&noinfo")
-    .then((res) => res.json()).then(data => setUsers(data.results)) 
+    .then((res) => res.json()).then(data => {
+        setUsers(data.results),
+        setFiltedUser(data.results)
+        setFilterNames(data.results)
+    }
+        
+        ) 
     },[]);
 
+    useEffect (() => {
+        const Reposição = users.map((repo) => { return repo.name.first})
+        const Search = Reposição.filter((repo) => repo.includes(storageName))
+        setFilterNames(Search);
+
+     //  const repositor = users.filter(item => item.name.first);
+      // const Search = repositor.filter(item => item.name.first.includes(storageName));
+     //  setFilterNames(Search);
+      // console.log(filterNames)
+    },[]);
     
 
     return (
@@ -37,8 +53,8 @@ export default function Home() {
                         <span>Insira um nome</span>
                     </div>
                     <input type="text" value={storageName}  placeholder="Nome" onChange={(e) => setStorageName(e.target.value)} />
-                    <button onClick={HandleName}>Pesquisar</button>
-                   {imprimir.map( (user) => <ContainerUsers key={user.picture}> 
+                    <button onClick={handleSearch}>Pesquisar</button>
+                   {filtedUser.map( (user) => <ContainerUsers key={user.email}> 
                             <div>
                             <img src={user.picture.thumbnail}  />
                             </div>
