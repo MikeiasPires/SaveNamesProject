4a29a1a72be3a90b4ceec2e7eae602cac6d6a003
Mike <mikeiaspires23@gmail.com>
2023-02-02 16:13:02 -0300

    mais um commit pra conta

--- src/Page/Home/index.tsx
+++ src/Page/Home/index.tsx
@@ -6,9 +6,11 @@ export default function Home() {
 
     const [storageName, setStorageName] = useState('')
     const [users, setUsers] = useState([])
-    const [usersten, setUsersten] = useState([])
     const [filtedUser, setFiltedUser] = useState([])
-    const [filterNames, setFilterNames] = useState([])
+    const [filterNames, setFilterNames] = useState([]);
+    const [filterNamesFity, setfilterNamesFity] = useState([]);
+    const [usersten, setUsersten] = useState(10)
+
     
         function handleSearch() {
         const filtered = users.filter(item => item.name.first === storageName);
@@ -20,37 +22,43 @@ export default function Home() {
         }  
         };
 
-        function Clickten(){
-            const comp = users.filter 
+
+        function selectionNumberUsers(e){
+            setUsersten(e.target.value);
+            if(usersten == 10){
+                setFiltedUser(filterNames);
+            }else if(usersten == 50){
+                setFiltedUser(filterNamesFity)
+             }else if(usersten == 100){
+            setFiltedUser(users)
+
+             }
+
         }
 
-       useEffect (() => {
-        const seach = users.filter(n => n.name.first === storageName);
-        setFilterNames(seach);
+           useEffect(()=>{
+            fetch("https://randomuser.me/api/?seed=javascript&results=10&nat=BR&noinfo")
+            .then((res) => res.json()).then(data => {
+                setFilterNames(data.results)
+            })
+           },[])
+        
+           useEffect(()=>{
+            fetch("https://randomuser.me/api/?seed=javascript&results=50&nat=BR&noinfo")
+            .then((res) => res.json()).then(data => {
+                setfilterNamesFity(data.results)
+            })
+           },[])
 
-       },[])
-
-        useEffect (() => {
+           useEffect (() => {
             fetch("https://randomuser.me/api/?seed=javascript&results=100&nat=BR&noinfo")
            .then((res) => res.json()).then(data => {
-            setUsers(data.results),
-            setFiltedUser(data.results)
-            setFilterNames(data.results)
+            setUsers(data.results)
+          //  setFiltedUser(data.results)
             }
-            
             ) 
             },[]);
-
-                useEffect (() => {
-                    fetch("https://randomuser.me/api/?seed=javascript&results=10&nat=BR&noinfo")
-                   .then((res) => res.json()).then(data => {
-                    setUsersten(data.results)
-                    }
-                    
-                    ) 
-                    },[]);
-    
- console.log(usersten)
+     
     return (
         <>
             <Container>
@@ -62,13 +70,13 @@ export default function Home() {
                     <button onClick={handleSearch}>Pesquisar</button>
                     <div>
                         <label >Quantidade</label>
-                            <select name="Quantidade" id="">
-                                <option onClick={Clickten}>10</option>
-                                <option value="">50</option>
-                                <option value="">100</option>
+                            <select name="Quantidade" onClick={selectionNumberUsers}>
+                                <option value="10">10</option>
+                                <option value="50">50</option>
+                                <option value="100">100</option>
                             </select>
                            </div>
-                   {  filterNames.map( (user) => <ContainerUsers key={user.email}> 
+                   {  filtedUser.map( (user) => <ContainerUsers key={user.email}> 
                             <div>
                             <img src={user.picture.thumbnail}  />
                             </div>
